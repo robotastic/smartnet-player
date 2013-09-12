41b7992ef23b23e2d07571a1d3b57bb12c583999
Robotastic <luke@robotastic.com>
2013-09-11 20:40:18 -0400

    template

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var fs = require('fs');
 var path = require('path');
 var config = require('./config.json');
 var Db = require('mongodb').Db,
-	Connection = require('mongodb').Connection,
-	Server = require('mongodb').Server;
+  Connection = require('mongodb').Connection,
+  Server = require('mongodb').Server;
 
 var host = process.env['MONGO_NODE_DRIVER_HOST'] != null ? process.env['MONGO_NODE_DRIVER_HOST'] : 'localhost';
 var port = process.env['MONGO_NODE_DRIVER_PORT'] != null ? process.env['MONGO_NODE_DRIVER_PORT'] : Connection.DEFAULT_PORT;
@@ -12,121 +12,40 @@ var scanner = new Db('scanner', new Server(host, port, {}));
 var db;
 
 scanner.open(function(err, scannerDb) {
-	db = scannerDb;
-       	scannerDb.authenticate(config.dbUser, config.dbPass, function() { });
+  db = scannerDb;
+  scannerDb.authenticate(config.dbUser, config.dbPass, function() {});
 });
 
 
 
 var app = express()
-function compile(str, path) {
-  return stylus(str)
-    .set('filename', path)
-    .use(nib())
-}
+
+  function compile(str, path) {
+    return stylus(str)
+      .set('filename', path)
+      .use(nib())
+  }
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
 app.use(express.logger('dev'))
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function (req, res) {
-  
-db.collection('transmissions', function(err, transCollection) {
-	text = '<head><link type="text/css" href="/skins/jplayer.blue.monday.css" rel="stylesheet" /><script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js"></script><script type="text/javascript" src="/js/jquery.jplayer.min.js"></script><body>
-  <div id="jquery_jplayer_1" class="jp-jplayer"></div>
-  <div id="jp_container_1" class="jp-audio">
-    <div class="jp-type-single">
-      <div class="jp-gui jp-interface">
-        <ul class="jp-controls">
-          <li><a href="javascript:;" class="jp-play" tabindex="1">play</a></li>
-          <li><a href="javascript:;" class="jp-pause" tabindex="1">pause</a></li>
-          <li><a href="javascript:;" class="jp-stop" tabindex="1">stop</a></li>
-          <li><a href="javascript:;" class="jp-mute" tabindex="1" title="mute">mute</a></li>
-          <li><a href="javascript:;" class="jp-unmute" tabindex="1" title="unmute">unmute</a></li>
-          <li><a href="javascript:;" class="jp-volume-max" tabindex="1" title="max volume">max volume</a></li>
-        </ul>
-        <div class="jp-progress">
-          <div class="jp-seek-bar">
-            <div class="jp-play-bar"></div>
-          </div>
-        </div>
-        <div class="jp-volume-bar">
-          <div class="jp-volume-bar-value"></div>
-        </div>
-        <div class="jp-time-holder">
-          <div class="jp-current-time"></div>
-          <div class="jp-duration"></div>
-          <ul class="jp-toggles">
-            <li><a href="javascript:;" class="jp-repeat" tabindex="1" title="repeat">repeat</a></li>
-            <li><a href="javascript:;" class="jp-repeat-off" tabindex="1" title="repeat off">repeat off</a></li>
-          </ul>
-        </div>
-      </div>
-      <div class="jp-title">
-        <ul>
-          <li>Bubble</li>
-        </ul>
-      </div>
-      <div class="jp-no-solution">
-        <span>Update Required</span>
-        To play the media you will need to either update your browser to a recent version or update your <a href="http://get.adobe.com/flashplayer/" target="_blank">Flash plugin</a>.
-      </div>
-    </div>
-  </div>
-</body></head>';
-	text = text + '<body>
-  <div id="jquery_jplayer_1" class="jp-jplayer"></div>
-  <div id="jp_container_1" class="jp-audio">
-    <div class="jp-type-single">
-      <div class="jp-gui jp-interface">
-        <ul class="jp-controls">
-          <li><a href="javascript:;" class="jp-play" tabindex="1">play</a></li>
-          <li><a href="javascript:;" class="jp-pause" tabindex="1">pause</a></li>
-          <li><a href="javascript:;" class="jp-stop" tabindex="1">stop</a></li>
-          <li><a href="javascript:;" class="jp-mute" tabindex="1" title="mute">mute</a></li>
-          <li><a href="javascript:;" class="jp-unmute" tabindex="1" title="unmute">unmute</a></li>
-          <li><a href="javascript:;" class="jp-volume-max" tabindex="1" title="max volume">max volume</a></li>
-        </ul>
-        <div class="jp-progress">
-          <div class="jp-seek-bar">
-            <div class="jp-play-bar"></div>
-          </div>
-        </div>
-        <div class="jp-volume-bar">
-          <div class="jp-volume-bar-value"></div>
-        </div>
-        <div class="jp-time-holder">
-          <div class="jp-current-time"></div>
-          <div class="jp-duration"></div>
-          <ul class="jp-toggles">
-            <li><a href="javascript:;" class="jp-repeat" tabindex="1" title="repeat">repeat</a></li>
-            <li><a href="javascript:;" class="jp-repeat-off" tabindex="1" title="repeat off">repeat off</a></li>
-          </ul>
-        </div>
-      </div>
-      <div class="jp-title">
-        <ul>
-          <li>Bubble</li>
-        </ul>
-      </div>
-      <div class="jp-no-solution">
-        <span>Update Required</span>
-        To play the media you will need to either update your browser to a recent version or update your <a href="http://get.adobe.com/flashplayer/" target="_blank">Flash plugin</a>.
-      </div>
-    </div>
-  </div>
-</body>'
-transCollection.find(function(err, cursor) {
-cursor.each(function(err, item) {
-	if (item) {
-	    text = text + "<li>Talkgroup: " + item.talkgroup + "</li>";
-	} else {
-	    text = text + "</ul></body></html>";
-	    res.send(text);
-	}
-});
-});
-});	
+app.get('/', function(req, res) {
+
+  db.collection('transmissions', function(err, transCollection) {
+    calls = [];
+
+    transCollection.find(function(err, cursor) {
+      cursor.each(function(err, item) {
+        if (item) {
+          calls.push(item.talkgroup);
+        } else {
+
+          res.render('player', calls);
+        }
+      });
     });
-app.listen(3004);	
+  });
+});
+app.listen(3004);
