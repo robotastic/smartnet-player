69aaa3f78fa34cf6b84803c84e1d0f473fbc7649
Robotastic <luke@robotastic.com>
2013-09-14 20:45:46 -0400

    fixer up

--- public/js/smartnet.js
+++ public/js/smartnet.js
@@ -1,12 +1,19 @@
 //var socket = io.connect('http://robotastic.com');
-        
-function print_call_row(filename, talkgroup) {
-	$("#call_list").append("<li><a href='/media/"+ filename +"'>"+ talkgroup + "</a></li>");
-}
-
 
 function play_call(filename) {
 	$("#jquery_jplayer_1").jPlayer("setMedia", {
       mp3: filename
     }).jPlayer("play");
 }
+        
+function print_call_row(filename, talkgroup) {
+	newdiv = document.createElement("div");
+	newdb.html(talkgroup);
+	newdiv.click(function() {play_call(filename) });
+	newli = document.createElement( "li" );
+	newli.append(newdiv);
+	
+	$("#call_list").append( newli  );
+}
+
+
