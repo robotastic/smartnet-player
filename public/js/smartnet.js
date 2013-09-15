02868242f416cf70dd410b06cd576fe132cfdc6c
Luke <luke@robotastic.com>
2013-09-14 21:50:03 -0400

    transfering

--- public/js/smartnet.js
+++ public/js/smartnet.js
@@ -1,16 +1,17 @@
 //var socket = io.connect('http://robotastic.com');
 
 function play_call(filename) {
+    console.log("trying to play: " + filename);
 	$("#jquery_jplayer_1").jPlayer("setMedia", {
       mp3: filename
     }).jPlayer("play");
 }
         
 function print_call_row(filename, talkgroup) {
-	newdiv = document.createElement("div");
-	newdb.html(talkgroup);
+    newdiv = $("<div/>");
+	newdiv.html(talkgroup);
 	newdiv.click(function() {play_call(filename) });
-	newli = document.createElement( "li" );
+    newli = $( "<li/>" );
 	newli.append(newdiv);
 	
 	$("#call_list").append( newli  );
