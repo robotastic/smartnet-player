01d5238e5956ea5d0fafcab43bbb7e017fa11a6e
Robotastic <luke@robotastic.com>
2013-09-22 14:35:00 -0400

    slow progress

--- index.js
+++ index.js
@@ -47,6 +47,26 @@ function compile(str, path) {
     .set('filename', path)
     .use(nib())
 }
+
+function build_filter(code, start_time) {
+ var filter = "";
+ switch(code) {
+  case 'group-fire':
+    filter =  "talkgroup: { $in: [1616,1632,1648,1680,1696,1712,1744,1760,1776,1808,1824,1840,1872,1888,1904,1920,1936,1952,1968,2000,2016,2048,2064,2080,2096,2112,2128,2144,2160,2176,2192,2224,2240,2272,2288,2304,2320,2336,2352,2368,2384,2400,2416,2432,2448,2464,2480,2496,2512,2592,2608,2640,2720,2736,2752,2848,2864,2880,9808,9824,9840,9872,9984,10032, 40000,40032] } }"; 
+  break;
+  case 'group-common':
+    filter = "talkgroup: { $in: [2656,2672,9936,9968,16624,19248,33616,33648,35536,35568,37456,37488,37648,37680,59952,59968] } }";
+  break;
+  case 'group-services':
+    filter = "talkgroup: { $in: [33840,33872,33904,34128,34192,34288,34320,34352,34384,34416,34448,34480,34512,34576,34608,34672,34800,34832,34864,35024,35056,35088,35152,35184,35216,35248,35408,35440, 35600,35664,36880,37040,37200,37232,37328,37456,37488,37648,37680,40080] } }";
+  break;  
+
+ }
+
+
+}
+
+
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
 app.use(express.logger('dev'))
@@ -73,6 +93,9 @@ app.get('/channels', function(req, res) {
 app.post('/calls', function(req, res) {
   console.log(req.body.offset);
   offset = req.body.offset;
+  filter_code = req.body.filter_code;
+  start_time = req.body.start_time;
+
   calls = [];
   db.collection('transmissions', function(err, transCollection) {
     transCollection.find().count(function(e, count) {
--- public/js/smartnet.js
+++ public/js/smartnet.js
@@ -26,6 +26,24 @@ function print_call_row(path, filename, talkgroup, len) {
 	$("#call_table").prepend(newrow);
 }
 
+function add_filters() {
+	var groups =  [  
+			{	name: 'Fire/EMS',
+				code: 'group-fire'
+			},
+			{	name: 'DC Common',
+				code: 'group-common'
+			},
+			{
+				name: 'Services',
+				code: 'group-services' 
+			}
+		];
+	for (var i = 0; i < groups.length; i++) {
+    	var group = groups[i];
+		$("#group-filter").append($('<li><a href="#">' + group.name + '</a></li>').click(function() {filter_calls(group.code)}));
+	}
+}
 function fetch_calls(offset) {
 	$.ajax({
 		url: "/calls",
@@ -60,7 +78,7 @@ function fetch_calls(offset) {
 				$("#pages").empty();
 				if (current_page>1) {
 					//html = '<li><a href="#">&laquo;</a></li>';
-				    $("#pages").append($('<li><a href="#">&laquo;</a></li>')).click(function() {fetch_calls(current_page-1)});
+				    $("#pages").append($('<li><a href="#">&laquo;</a></li>').click(function() {fetch_calls(current_page-1)}));
 				} else {
 					//html = '<li class="disabled"><a href="#">&laquo;</a></li>';
 				    $("#pages").append($('<li class="disabled"><a href="#">&laquo;</a></li>'));
@@ -71,13 +89,13 @@ function fetch_calls(offset) {
 					} else {
 						if (page == current_page) {
 							//html = html + '<li class="active"><a href="#">'+ page + '</a></li>';
-						    $("#pages").append($('<li class="active"><a href="#">'+ page + '</a></li>')).click(function() {fetch_calls(page)} );
+						    $("#pages").append($('<li class="active"><a href="#">'+ page + '</a></li>').click(function() {fetch_calls(page)} ));
 						} else {
 							if (((page-1) * per_page) > count) {
 								break;
 							} else { 
 								//html = html + '<li><a href="#">'+ page + '</a></li>';
-							    $("#pages").append($('<li><a href="#">'+ page + '</a></li>')).click(function() {fetch_calls(page)} );
+							    $("#pages").append($('<li><a href="#">'+ page + '</a></li>').click(function() {fetch_calls(page)} ));
 						
 							}
 
@@ -88,7 +106,7 @@ function fetch_calls(offset) {
 				}
 				if ((page*per_page) < count) {
 					//html = html + '<li><a href="#">&raquo;</a></li>';
-				    $("#pages").append($('<li><a href="#">&raquo;</a></li>')).click( function() {fetch_calls(current_page+1)} );
+				    $("#pages").append($('<li><a href="#">&raquo;</a></li>').click( function() {fetch_calls(current_page+1)} ));
 				
 				} else {
 					//html = html + '<li class="disabled"><a href="#">&raquo;</a></li>';
