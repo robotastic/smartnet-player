01d32e5f94b0f650d2ed9f0b3f8c4de674d06340
Robotastic <luke@robotastic.com>
2013-10-09 11:26:40 -0400

    yep, I was changing the wrong spot

--- index.js
+++ index.js
@@ -330,7 +330,7 @@ app.use(express.bodyParser());
 
 app.get('/', function(req, res) {
   var filter_code = "";
-  var filter_date = "";
+  var filter_date = '""';
   res.render('player', {
     filter_date: filter_date,
     filter_code: filter_code
@@ -341,7 +341,7 @@ app.post('/', function(req, res) {
   var filter_code = req.body.filter_code;
   if (!filter_code) filter_code = "";
   var filter_date = "new Date('" + req.body.filter_date + "');";
-  if (!filter_date) filter_date = "'none'";
+  if (!filter_date) filter_date = '""';
 
   res.render('player', {
     filter_date: filter_date,
