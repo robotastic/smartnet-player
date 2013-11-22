08fa260966ace4e2bcae397a37bd31aa22ce09f8
robotastic <luke@robotastic.com>
2013-11-21 21:37:02 -0500

    paran

--- public/js/smartnet.js
+++ public/js/smartnet.js
@@ -179,10 +179,10 @@ function print_call_row(call, direction, live) {
 	var callview = $('<a href="/call/' + call.objectId + '"><i class="icon-file call-link"> </i></a><a href="/call/' + call.objectId + '"><span class="glyphicon glyphicon-link call-link"></span></a>');
 	var linkview = $('<i class="icon-share-alt"> </i><span class="glyphicon glyphicon-cloud-upload"></span>');
 	if (calls.stars == 0 ) {
-	var starbutton = $('<i class="icon-star-empty icon-star-button"> </i><span class="glyphicon glyphicon-star-empty glyphicon-star-button"></span>';
+	var starbutton = $('<i class="icon-star-empty icon-star-button"> </i><span class="glyphicon glyphicon-star-empty glyphicon-star-button"></span>');
 	var	starcount = $('<span class="star-count"></span>');
 	} else {
-	var starbutton = $('<i class="icon-star icon-star-button"> </i><span class="glyphicon glyphicon-star glyphicon-star-button"></span>';
+	var starbutton = $('<i class="icon-star icon-star-button"> </i><span class="glyphicon glyphicon-star glyphicon-star-button"></span>');
 	var	starcount = $('<span class="star-count">' + call.stars + '</span>');
 	}
 	starbutton.click(function() {
