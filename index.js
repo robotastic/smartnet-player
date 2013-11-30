81f99bbd202079b7c0bec3d3b267a60d17a50899
robotastic <luke@robotastic.com>
2013-11-30 14:43:57 -0500

    serialize user

--- index.js
+++ index.js
@@ -208,7 +208,20 @@ app.use(express.logger('dev'))
 app.use(express.static(__dirname + '/public'));
 
 
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete Twitter profile is serialized
+//   and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
 
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
 
 // Use the TwitterStrategy within Passport.
 //   Strategies in passport require a `verify` function, which accept
