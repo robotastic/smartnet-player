e299c40a0e9bb2725512a57484bb3e57196d02f2
robotastic <luke@robotastic.com>
2014-02-23 21:54:39 -0500

    hmm

--- public/js/smartnet-beta.js
+++ public/js/smartnet-beta.js
@@ -66,7 +66,7 @@ if(typeof console === "undefined") {
 }
 
 function twitter_success(user) {
-	$('#user-bar').html('<img src="' + user.photos[0].value + '" class="img-circle, pull-right">');
+	$('#user-bar').html('<img src="' + user.photos[0].value + '" class="img-circle pull-right">');
 }
 
 function tweet_call(tweet) {
