9e7a40c65f703d38b59cb4bea741168d8a6bb432
robotastic <luke@robotastic.com>
2015-04-05 14:59:39 -0400

    CORS

--- index.js
+++ index.js
@@ -553,7 +553,12 @@ function ensureAuthenticated(req, res, next) {
 
 
 
-
+app.all('*', function(req, res, next) {
+       res.header("Access-Control-Allow-Origin", "*");
+       res.header("Access-Control-Allow-Headers", "X-Requested-With");
+       res.header('Access-Control-Allow-Headers', 'Content-Type');
+       next();
+});
 
 
 app.get('/about', function(req, res) {
